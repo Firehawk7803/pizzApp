d32de49d315d09be38b2a7909562c322dfbfde6a
Kurosh Khaliqi <kurosh.khaliqi@gmail.com>
2022-03-07 15:33:55 -0500

    Design finished

---
+++ script.js
@@ -0,0 +1,9 @@
+let product1NameElement = document.getElementById("product1-name");
+//selecet the product1-ingredeients and product1-price
+//select the other products
+
+function onSaladClick() {
+  //change the product1-ingredeient and product1-price
+  product1NameElement.innerText = "Lasagna";
+  //change the other products to salads
+}
